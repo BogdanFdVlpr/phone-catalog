2fb967052a900bda8da2ba076c4ad12c035b845d
Bogdan Prokopenko <bogdanprokopenko67@gmail.com>
2023-08-29 22:11:26 +0300

    add select for quantity goods, add small navigation for goods component

--- src/app/components/small-navigation/small-navigation.component.ts
+++ src/app/components/small-navigation/small-navigation.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-small-navigation',
   templateUrl: './small-navigation.component.html',
   styleUrls: ['./small-navigation.component.scss']
 })
-export class SmallNavigationComponent {
+export class SmallNavigationComponent{
 
+  constructor() {}
+
+  headerLink = window.location.href.split('/', ).splice(3, 4)
 }
--- src/app/pages/tablets-page/tablets-page.component.ts
+++ src/app/pages/tablets-page/tablets-page.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./tablets-page.component.scss']
 })
 export class TabletsPageComponent {
-
+  // isLoading: boolean = true;
 }
